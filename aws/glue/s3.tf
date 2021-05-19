35f984696005d319e4871595457c00ec6cb218d6
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2021-05-19 07:26:51 -0300

    stock files

---
+++ aws/glue/s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "investing_bucket" {
+  bucket = "ortisan-investing-data-bucket"
+  acl    = "private"
+  tags = {
+    Name        = "Glue Testing Demo Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "local_file" "timestamp" {
+  filename = "timestamp"
+  content = "${timestamp()}"
+}
+
+resource "aws_s3_bucket_object" "stocks" {
+  for_each = fileset("${path.module}/s3-files/stocks/", "*")
+  bucket = aws_s3_bucket.investing_bucket.id
+  key = "stocks/${each.value}"
+  source = "s3-files/stocks/${each.value}"
+  etag = filemd5("${path.module}/s3-files/stocks/${each.value}")
+}
